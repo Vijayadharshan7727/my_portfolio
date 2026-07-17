9fe479acbaa3656d4c5fda91c4ca018c4db8624d
VIJAYADHARSHAN R <dharshanvijay7727@gmail.com>
2026-07-17 14:35:05 +0530

    Create education.py to display education journey
    Add education section with timeline of academic achievements.

---
+++ pages/education.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+def education():
+
+    st.markdown("<div class='section'>🎓 Education Journey</div>", unsafe_allow_html=True)
+
+    st.markdown("""
+
+<div class="timeline">
+
+<h3>2024 - Present</h3>
+
+<h2>B.Tech Artificial Intelligence & Data Science</h2>
+
+<p>
+Rathinam Technical Campus
+<br>
+Coimbatore
+</p>
+
+</div>
+
+""", unsafe_allow_html=True)
+
+    st.markdown("""
+
+<div class="timeline">
+
+<h3>2023 - 2024</h3>
+
+<h2>Higher Secondary (+2)</h2>
+
+<p>Karthi Vidhyala Matric Higher Secondary School</p>
+
+</div>
+
+""", unsafe_allow_html=True)
+
+    st.markdown("""
+
+<div class="timeline">
+
+<h3>2021 - 2022</h3>
+
+<h2>SSLC</h2>
+
+<p>Karthi Vidhyala Matric Higher Secondary School</p>
+
+</div>
+
+""", unsafe_allow_html=True)
